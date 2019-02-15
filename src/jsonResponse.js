72296fa7659ae763ab6d551dbacab3d747839533
lampitosgames <dantimko1@gmail.com>
2019-02-14 20:43:50 -0500

    Build now passing ESLint tests

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -1,31 +1,31 @@
-//User storage variable
-let users = { users: {} };
+// User storage variable
+const users = { users: {} };
 
 const getUsers = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/json' });
   res.write(JSON.stringify(users));
   res.end();
-}
+};
 
 const addUser = (req, res, body) => {
-  if (users["users"].hasOwnProperty(body.name)) {
-    users["users"][body.name]["age"] = body.age;
+  if (Object.prototype.hasOwnProperty.call(users.users, body.name)) {
+    users.users[body.name].age = body.age;
     res.writeHead(204);
     res.end();
   } else {
-    users["users"][body.name] = body;
-    res.writeHead(201, { "Content-Type": "text/json" });
+    users.users[body.name] = body;
+    res.writeHead(201, { 'Content-Type': 'text/json' });
     res.write(JSON.stringify({
-      message: "Created successfully"
+      message: 'Created successfully',
     }));
     res.end();
   }
-}
+};
 
 const sendError = (req, res, errorNumber, id, message) => {
   res.writeHead(errorNumber, { 'Content-Type': 'text/json' });
   res.write(JSON.stringify({ id, message }));
   res.end();
-}
+};
 
 module.exports = { getUsers, addUser, sendError };
--- src/router.js
+++ src/router.js
@@ -3,42 +3,39 @@ const { getIndex, getCSS } = require('./htmlResponse.js');
 const { getUsers, addUser, sendError } = require('./jsonResponse.js');
 
 const onRequest = (req, res) => {
-  let { method, url } = req;
+  const { method, url } = req;
   let body = [];
-  req.on('error', (err) => console.error(err)).on('data', (chunk) => body.push(chunk)).on('end', () => {
-    //Handle response errors
-    res.on('error', (err) => console.error(err));
-
-    //Route selection for get/head requests
+  req.on('data', chunk => body.push(chunk)).on('end', () => {
+    // Route selection for get/head requests
     if (method === 'GET' || method === 'HEAD') {
       switch (url) {
-      case '/':
-        getIndex(req, res);
-        break;
-      case '/style.css':
-        getCSS(req, res);
-        break;
-      case '/getUsers':
-        getUsers(req, res);
-        break;
-      default:
-        sendError(req, res, 404, "notFound", "The page/resource you are looking for cannot be found");
-        break;
+        case '/':
+          getIndex(req, res);
+          break;
+        case '/style.css':
+          getCSS(req, res);
+          break;
+        case '/getUsers':
+          getUsers(req, res);
+          break;
+        default:
+          sendError(req, res, 404, 'notFound', 'The page/resource you are looking for cannot be found');
+          break;
       }
     } else if (method === 'POST') {
-      //Parse body of request
+      // Parse body of request
       body = parse(Buffer.concat(body).toString());
       switch (url) {
-      case '/addUser':
-        if (body.age === '' || body.name === '') {
-          sendError(req, res, 400, "missingParams", "Name and age are both required.");
-          return;
-        }
-        addUser(req, res, body);
-        break;
-      default:
-        sendError(req, res, 404, "notFound", "POST API endpoint not found");
-        break;
+        case '/addUser':
+          if (body.age === '' || body.name === '') {
+            sendError(req, res, 400, 'missingParams', 'Name and age are both required.');
+            return;
+          }
+          addUser(req, res, body);
+          break;
+        default:
+          sendError(req, res, 404, 'notFound', 'POST API endpoint not found');
+          break;
       }
     }
   });
